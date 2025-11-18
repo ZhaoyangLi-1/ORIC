ed23977c2fdc22d3405331a98c5262bd23cde0a5
ZhaoyangLi-1 <zhl165@ucsd.edu>
2025-11-18 08:05:26 +0000

    latest

--- virft/open_r1/trainer/__init__.py
+++ virft/open_r1/trainer/__init__.py
@@ -1,11 +1,7 @@
 from .grpo_trainer import Qwen3VLGRPOTrainer
 from .vllm_grpo_trainer import Qwen3VLGRPOVLLMTrainer
-from .grpo_trainer_mp import Qwen3VLGRPOTrainer_MP
-from .grpo_trainer_aid import Qwen3VLGRPOTrainer_AID
 
 __all__ = [
     "Qwen3VLGRPOTrainer",
     "Qwen3VLGRPOVLLMTrainer",
-    "Qwen3VLGRPOTrainer_MP",
-    "Qwen3VLGRPOTrainer_AID",
 ]
